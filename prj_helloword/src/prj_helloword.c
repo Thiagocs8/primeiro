309354efbeac3203c19ce7f936d595773cc4c3f8
Thiago Costa Soares <trigocs64@gmail.com>
2024-02-26 12:14:15 -0300

    Add files via upload

---
+++ prj_helloword/src/prj_helloword.c
@@ -0,0 +1,31 @@
+/*
+ ============================================================================
+ Name        : prj_helloword.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define PI 3.14159
+
+int main(void) {
+
+	int idade = 26;
+	const int MAX_NUM = 100;
+
+	printf("Minha variável int comum = %d\n" , idade);
+	printf("Minha variável int constante = %d\n", MAX_NUM);
+	printf("Minha constante = %f\n" , PI);
+
+
+
+	return 0;
+}
+
+
+
